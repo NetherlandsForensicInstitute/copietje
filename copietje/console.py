f4e41272d4bc07c9619e59e518ad612c5766354b
Mattijs Ugen <144798+akaIDIOT@users.noreply.github.com>
2024-10-30 09:57:58 +0100

    Remove TODO, resume logic has been added

--- copietje/console.py
+++ copietje/console.py
@@ -100,8 +100,6 @@ def download(*, context, database, target=None, limit=None, condenser=None, jobs
         # search hansken for the documents to download + minhash
         documents = context.search(Term('type', 'document'), count=limit)
         # issue bulk download with side effects to store all the documents and the minhashes
-        # TODO: should the download crash halfway, how do we resume it? It's being downloaded in parallel, there's no
-        #       clear point to restart from... (should be doable if the database inserts can be done idempotent)
         LOG.info('starting bulk download of %d results (limited to %d)', documents.num_results, limit)
         export.bulk(documents, target,
                     stream=partial(determine_stream, database=database),
